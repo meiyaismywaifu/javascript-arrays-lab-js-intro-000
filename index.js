a6ea74cfd4a2f7c3f00bb4d0ff7f04b3a3ad74f9
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-06-28 23:56:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(name){
-
-}
-function destructivelyPrependKitten(name){
-
-}
-function destructivelyRemoveLastKitten(name){
-
-}
-function destructivelyRemoveFirstKitten(name){
-
-}
 function appendKitten(name){
 
 }
@@ -25,3 +13,19 @@ function removeLastKitten(name){
 function removeFirstKitten(name){
 
 }
+
+// --- --- --- //
+
+function destructivelyAppendKitten(name){
+
+}
+function destructivelyPrependKitten(name){
+
+}
+function destructivelyRemoveLastKitten(name){
+
+}
+function destructivelyRemoveFirstKitten(name){
+
+}
+
