dee8776de4722b53a8e127111ff2f2315d4022a7
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-06-28 23:54:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ var kittens = [] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-  
+
 }
 function destructivelyPrependKitten(name){
-  
+
 }
 function destructivelyRemoveLastKitten(name){
-  
+
 }
 function destructivelyRemoveFirstKitten(name){
-  
+
 }
 function appendKitten(name){
-  
+
 }
 function prependKitten(name){
-  
+
 }
 function removeLastKitten(name){
-  
+
 }
 function removeFirstKitten(name){
-  
+
 }
